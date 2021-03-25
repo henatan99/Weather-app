312512212ee24254a71801775751d1ff2745b47d
Henok <henatan99@gmail.com>
2021-03-25 13:16:56 +0300

    - adds webpack.config.js file in the roots with index.js as entry and main.js as output - adds main.js as source in the script tag index.html file

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'none',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  devServer: {
+    contentBase: './dist',
+  },
+
+  module: {
+    
+  },
+
+};
